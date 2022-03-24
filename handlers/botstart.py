0c7ba118223aaaedb35893f33db8d9bfcf7ae5fa
user name <98596498+vinuth2@users.noreply.github.com>
2022-03-25 05:23:35 +0530

    Update botstart.py

--- handlers/botstart.py
+++ handlers/botstart.py
@@ -52,8 +52,10 @@ async def _human_time_duration(seconds):
 )
 async def start_private(client: Client, message: Message):
     await message.reply_text(
-        f"""Hey there! I'm [Stella](https://telegra.ph/file/cc41cf2eebde1c4c0a0dd.jpg) ✨
-I Play and Download music on Telegram. To know how to use me, click on the Initial Setup button below!""",
+        f"""────「 [Princess Stella]((https://telegra.ph/file/cc41cf2eebde1c4c0a0dd.jpg) 」────
+
+**Hey there! I'm 𝓢𝓽𝓮𝓵𝓵𝓪** ✨
+I Play and Download music on Telegram. To know how to use me, click on the **Initial Setup** button below!""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
